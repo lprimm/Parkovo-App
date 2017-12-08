8bad16616e81978e7d0d3b51cb29f4a0985df485
lprimm <lpprimm@gmail.com>
2017-12-08 09:52:50 -0600

    Initial Commit

---
+++ Parkovo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Parkovo
+//
+//  Created by Luke Primm on 12/8/17.
+//  Copyright © 2017 Freed-Hardeman University. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
